afd50a002b88c2ef090aa6ca71b2e52bff4cc72e
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-11-14 19:34:55 -0500

    feat: script to update and upgrade

---
+++ check.sh
@@ -0,0 +1,9 @@
+# run update
+
+echo "Updating..." | lolcat
+sudo apt-get update
+
+# run upgrade
+
+echo "Upgrading..." | lolact
+sudo apt-get upgrade
---
+++ first.sh
@@ -0,0 +1,17 @@
+echo "Update the system"
+
+sudo apt-get update
+
+echo "Upgrade the system"
+
+sudo apt-get upgrade
+
+echo "Update and upgrade complete"
+
+# install lolcat
+
+echo "Installing lolcat"
+
+sudo apt-get install lolcat
+
+echo "Installation complete" | lolcat
